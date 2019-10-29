0e2481fd3e09bdb608e001701c29ff291a1ce5e5
Tony <tonyywlau@gmail.com>
2019-10-29 16:52:45 -0700

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,70 @@
+let gameObjects = 
+{
+    tileBool: []
+};
+
+function initialize() 
+{
+    let element = document.createElement("p");
+    element.setAttribute("id", "game-title");
+    element.innerText = "Welcome to Matrix Memory";
+    document.body.appendChild(element);
+
+    element = document.createElement("div");
+    element.setAttribute("id", "game-board");
+    document.body.appendChild(element);
+
+    element = document.createElement("button");
+    element.setAttribute("id", "start-button");
+    element.innerText = "Start";
+    document.body.appendChild(element);
+
+    layTiles(25);
+    
+}
+
+function layTiles(numOfTile) 
+{
+    let board = document.getElementById("game-board");
+    for (let i = 0; i < numOfTile; i++)
+    {
+        let tile = document.createElement("div");
+        tile.setAttribute("id", i);
+        tile.setAttribute("class", "tile");
+
+        board.appendChild(tile);
+    }    
+}
+
+function randomize(numOfTrue, numOfTile) 
+{
+    let trueTiles = [], tileChart =[];
+    for (let tileID = 0; tileID < numOfTile; tileID++) 
+    {
+        tileChart[tileID] = tileID;
+    }
+    for (let i = 0; i < numOfTrue; i++) 
+    {
+        let TTile = Math.random() * numOfTile;
+        trueTiles[i] = TTile;
+        tileChart[TTile] = tileChart[numOfTile - i];
+        tileChart[numOfTile - i] = TTile;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
