ee4ffbeb47430a022f208fd09391e2febad9c4e3
Tony <tonyywlau@gmail.com>
2019-10-29 18:39:53 -0700

    added flip divs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,9 @@
 let gameObjects = 
 {
-    tileBool: []
+    tileBool: [],
+    trueTiles: [],
+    numOfTile: 25,
+    numOfTrue: 5
 };
 
 function initialize() 
@@ -14,44 +17,84 @@ function initialize()
     element.setAttribute("id", "game-board");
     document.body.appendChild(element);
 
-    element = document.createElement("button");
-    element.setAttribute("id", "start-button");
-    element.innerText = "Start";
-    document.body.appendChild(element);
+    // element = document.createElement("button");
+    // element.setAttribute("id", "start-button");
+    // element.innerText = "Start";
+    // document.body.appendChild(element);
 
-    layTiles(25);
-    randomize(5, 25);
+    startGame();
+    
     
 }
 
-function layTiles(numOfTile) 
+function startGame() 
+{
+    // while (true) 
+    // {
+        layTiles();
+        randomize();
+        
+        // for (let i = 0; i < numOfTile; i++) 
+        // {
+            
+        // }
+        
+    // }
+
+}
+
+function showTrueTiles() 
+{
+    // for (let i = 0; i < numOfTrue; i++)
+    // {
+    //     let element = document.getElementById(gameObjects.numOfTrue[i]);
+    //     element.
+    // }
+}
+
+function layTiles() 
 {
     let board = document.getElementById("game-board");
-    for (let i = 0; i < numOfTile; i++)
+    for (let i = 0; i < gameObjects.numOfTile; i++)
     {
         let tile = document.createElement("div");
         tile.setAttribute("id", i);
         tile.setAttribute("class", "tile");
 
+        let tileInner = document.createElement("div");
+        tileInner.setAttribute("id", i + "-inner");
+        tileInner.setAttribute("class", "tile-inner");
+
+        let tileFront = document.createElement("div");
+        tileFront.setAttribute("id", i + "-front");
+        tileFront.setAttribute("class", "tile-front");
+
+        let tileBack = document.createElement("div");
+        tileBack.setAttribute("id", i + "-back");
+        tileBack.setAttribute("class", "-back-false");
+        
+        tileInner.appendChild(tileFront);
+        tileInner.appendChild(tileBack);
+        tile.appendChild(tileInner);
         board.appendChild(tile);
     }    
 }
 
-function randomize(numOfTrue, numOfTile) 
+function randomize() 
 {
-    let trueTiles = [], tileChart =[];
-    for (let tileID = 0; tileID < numOfTile; tileID++) 
+    let tileChart =[];
+    for (let tileID = 0; tileID < gameObjects.numOfTile; tileID++) 
     {
         tileChart[tileID] = tileID;
         gameObjects.tileBool[tileID] = false;
 
     }
-    for (let i = 0; i < numOfTrue; i++) 
+    for (let i = 0; i < gameObjects.numOfTrue; i++) 
     {
-        let TTile = Math.round(Math.random() * numOfTile);
-        trueTiles[i] = TTile;
-        tileChart[TTile] = tileChart[numOfTile - i];
-        tileChart[numOfTile - i] = TTile;
+        let TTile = Math.round(Math.random() * gameObjects.numOfTile);
+        gameObjects.trueTiles[i] = TTile;
+        tileChart[TTile] = tileChart[gameObjects.numOfTile - i];
+        tileChart[gameObjects.numOfTile - i] = TTile;
         gameObjects.tileBool[TTile] = true;
     }
 }
